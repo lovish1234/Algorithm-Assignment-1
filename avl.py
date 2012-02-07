9d7f567b8ad5be467818605456221e8e2f546e3b
Lovish Kumar <lovish1234@gmail.com>
2012-02-07 22:52:05 +0530

    rotation not working properly

--- avl.py
+++ avl.py
@@ -7,13 +7,15 @@ class Node :
 		self.left=leftchild
 		self.right=rightchild
 	def bfactor(self):
-		l=0
+		lheight=0
 		if self.left:
-			l=self.left.height()
-		r=0
+			lheight=self.left.height()
+		rheight=0
 		if self.right:
-			r=self.right.height()	
-		return (l - r)
+			rheight=self.right.height()
+#		print lheight
+#		print rheight	
+		return (lheight - rheight)
 	def height(self):
 		lheight=0
 		if self.left :
@@ -46,13 +48,14 @@ class Node :
 # Balancing the tree using above set of rotations		 
 	def balance(self):
 		value=self.bfactor()
+#		print value
 		if value >= 2:
 			if self.left.balance() >0:
 				self.rotate_right()
 			else:
 				self.rotate_left_right()
 		elif value<=(-2):
-			if self.right.balance() > 0:
+			if self.right.balance() < 0:
 				self.rotate_left()
 			else:
 				self.rotate_right_left()
@@ -70,23 +73,52 @@ class Node :
 				self.right = Node(data)
 			else:
 				self.right.Insert(data)
-#		self.balance()
-  
+		self.balance()
+  	
+	def Search(self,data):
+		if data==self.data:
+			print "Yes it exists"
+		elif data<=self.data and not self.left==None:
+			self.left.Search(data)
+		elif data > self.data and not self.right==None:
+			self.right.Search(data)
+		else:
+			print "It does not exist"
+
 	def inorder(self):
-		print  str(self.data)
+#		print  str(self.data)
 		if self.left:
 			self.left.inorder()
-#		print " "  * indent + str(self.data)
+		print  str(self.data)
 		if self.right:
 			self.right.inorder()	
 
-if __name__ == "__main__" :
-#	print "hello"
-	tree = Node(5)	
-	tree.Insert(7)
-	tree.Insert(1)
-	tree.Insert(2)
-	tree.Insert(4)
+
+#def Delete(self,datum):
+#		if self.data==datum:
+#Remove this node
+#		elif self.data<=datum:
+#			self.left.Delete(datum)
+#		elif self.data>datum:
+#			self.right.Delete(datum)
+			
+			
+				
+if __name__ == "__main__" :	
+	tree = Node(14)
+	tree.Insert(22)
+	tree.Insert(27)
+	tree.Insert(30)
+	tree.Insert(33)
+	tree.Insert(34)
+	tree.Insert(35)
+	tree.Insert(36)
+	tree.Insert(40)
+	tree.Insert(42)
+	tree.Insert(55)
+	tree.Insert(58)
+	tree.Search(41)
+	tree.Search(40)
 	tree.inorder()
 		
 
